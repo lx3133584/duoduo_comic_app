6ab0b164dd65a4c24cb0a80d3d95abbe486bab08
Liang Xu <liangxu@nle-tech.com>
2018-03-10 19:36:36 +0800

    完成漫画详情顶部模块

--- app/modules/comic/comic_detail/comic_detail_top/component.js
+++ app/modules/comic/comic_detail/comic_detail_top/component.js
@@ -1,21 +1,54 @@
 import React, { PureComponent } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import PropTypes from 'prop-types';
-import { Image } from 'react-native';
 import styled from "styled-components";
+import { BlurView } from 'react-native-blur';
+import { Image, Dimensions, findNodeHandle } from 'react-native';
+const { width, height } = Dimensions.get('window');
 
 const ContainStyled = styled.View`
-  height: 150px;
+  height: 180px;
 `
-const TopStyled = styled.View`
-  width: 80px;
-`
-const BottomStyled = styled.View`
-  margin-top: 8px;
+const coverImageStyled = {
+  position: 'absolute',
+  bottom: 10,
+  right: 10,
+  height: 120,
+  width: 85,
+  zIndex: 2,
+  borderWidth: 1,
+  borderColor: '#fff',
+}
+const blurImageStyled = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width,
+  height: 180,
+  zIndex: 1,
+}
+const TextContainStyled = styled.View`
+  position: absolute;
+  top: 60px;
+  left: 20px;
+  bottom: 15px;
+  z-index: 3;
 `
 const TitleStyled = styled.Text`
-  color: #000;
-  font-size: 14px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  margin-bottom: 8px;
+`
+const BottomTextContainStyled = styled.View`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+`
+const BottomTextStyled = styled.Text`
+  color: #fff;
+  font-size: 12px;
+  opacity: 0.8;
 `
 
 class ComicDetailComponent extends PureComponent {
@@ -28,22 +61,53 @@ class ComicDetailComponent extends PureComponent {
       }),
     }),
   };
+  state = {
+    viewRef: null,
+  };
   constructor() {
     super();
     this.onFetch = this.onFetch.bind(this);
+    this.imageLoaded = this.imageLoaded.bind(this)
   };
   componentDidMount() {
     const { id } = this.props.navigation.state.params;
     this.onFetch(id);
   };
+  imageLoaded() {
+    this.setState({ viewRef: findNodeHandle(this.backgroundImage) });
+  }
   async onFetch(id) {
     const { getDetail } = this.props;
     return await getDetail(id);
   };
   render() {
     const detail = this.props.detail.toJS();
+    const { viewRef } = this.state;
     return (
       <ContainStyled>
+        <Image
+          ref={(img) => { this.backgroundImage = img }}
+          style={blurImageStyled}
+          onLoadEnd={this.imageLoaded}
+          source={{uri: detail.cover}}
+        />
+        <BlurView
+          style={blurImageStyled}
+          viewRef={viewRef}
+          blurType="dark"
+          blurAmount={6}
+        />
+        <Image
+          style={coverImageStyled}
+          source={{uri: detail.cover}}
+        />
+        <TextContainStyled>
+          <TitleStyled>{detail.title}</TitleStyled>
+          <BottomTextStyled>{detail.author}</BottomTextStyled>
+          <BottomTextContainStyled>
+            <BottomTextStyled>人气 {detail.popularity_number || 0}</BottomTextStyled>
+          </BottomTextContainStyled>
+        </TextContainStyled>
       </ContainStyled>
     );
   }
