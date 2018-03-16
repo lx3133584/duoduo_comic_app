5036909e304090462cfe1cbdacbe4946e8d9f35c
Liang Xu <liangxu@nle-tech.com>
2018-03-16 16:00:28 +0800

    更换默认头像;修改登录页logo

--- app/modules/user/login/brand/component.js
+++ app/modules/user/login/brand/component.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import styled from "styled-components";
 import { brand_primary } from '../../../../theme';
+import { Image } from 'react-native';
 
 const ContainStyled = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  height: 300px;
+  height: 400px;
+`
+const ImageContainStyled = styled.View`
+  background-color: ${brand_primary};
+  border-radius: 999px;
+  justify-content: center;
+  align-items: center;
+  width: 72;
+  height: 72;
 `
 const TextStyled = styled.Text`
+  margin: 10px 0;
   text-align: center;
-  font-size: 24px;
+  font-size: 20px;
   font-weight: 900;
   color: ${brand_primary};
 `
+const imageStyle = {
+  width: 60,
+  height: 60,
+}
 
 export default function BrandComponent({ navigation }) {
   return (
     <ContainStyled>
+      <ImageContainStyled>
+        <Image
+          source={require('./logo.png')}
+          style={imageStyle}
+        />
+      </ImageContainStyled>
       <TextStyled>多多漫画</TextStyled>
     </ContainStyled>
   );
