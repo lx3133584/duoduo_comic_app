a1397aa16a2470e8e31fc13f44751e1a196ffe14
liangxu <liangxu@nle-tech.com>
2018-04-03 21:45:48 +0800

    添加头像显示

--- app/modules/user/user_info/avatar/component.js
+++ app/modules/user/user_info/avatar/component.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Avatar } from 'react-native-elements';
+import baseURL from '../../../../api/base_url';
 
 export default function AvatarComponent({ src }) {
   return (
     <Avatar
       large
       rounded
-      source={src ? { uri: src } : require('./avatar.jpg')}
+      source={src ? { uri: baseURL + src } : require('./avatar.jpg')}
       onPress={() => console.log("Works!")}
       activeOpacity={0.7}
     />
